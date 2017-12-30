e0b46a28bc7bfde806413a311db93bb1a6717f57
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-12-30 12:26:03 -0500

    Encapsulamiento, getters y setters en PHP

--- lesson2/index.php
+++ lesson2/index.php
@@ -4,22 +4,50 @@
 */
 
 class Person {
-
-  public $firstName;
-  public $lastName;
+  protected $firstName; // public, protected <-> private
+  protected $lastName;
+  protected $nickname;
+  protected $changedNickname = 0;
 
   public function __construct($firstName, $lastName){
     $this->firstName = $firstName;
     $this->lastName = $lastName;
   }
 
-  public function fullName(){
-    return $this->firstName.' '.$this->lastName;
+  public function getFirstName(){
+    return $this->firstName;
   }
 
+  public function getLastName(){
+    return $this->lastName;
+  }
+
+  public function setNickname($nickname) {
+    if ($this->changedNickname >= 2) {
+      throw new Exception(
+        "You can't change a nickname more than 2 times"
+      );
+    }
+
+    $this->nickname = $nickname;
+
+    $this->changedNickname++;
+  }
+
+  public function getNickname(){
+    return $this->nickname;
+  }
+
+  public function getFullName(){
+    return $this->firstName . ' ' . $this->lastName;
+  }
 }
 
-$person1 = new Person('Jose', 'Miguel');
-$person2 = new Person('Kevin', 'Andres');
+$person1 = new Person('Jonny', 'Alexander');
 
-echo "{$person1->fullName()} es amigo de {$person2->fullName()}";
+$person1->setNickname('jonnyalexbh');
+$person1->setNickname('jonnyalex.bh');
+
+exit($person1->getNickname());
+
+echo $person1->getFullName();
